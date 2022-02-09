f4bd2e3f296db2a5038d61b9d216c515fdacd934
Alexandre Vaz <alexandrevaz332@gmail.com>
2022-02-09 10:23:31 -0300

    First commit

---
+++ Aula/src/VendaCarro.java
@@ -0,0 +1,43 @@
+public class VendaCarro {
+    
+    private String cor;
+
+    private String modelo;
+
+    private int placa;
+
+    private String marca;
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getPlaca() {
+        return this.placa;
+    }
+
+    public void setPlaca(int placa) {
+        this.placa = placa;
+    }
+
+}
